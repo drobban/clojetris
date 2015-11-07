c709486873664c4a219ff72feb9ed4c6c630877b
David Robertsson <David.robertsson@gmail.com>
2015-11-07 11:50:23 +0100

    Error in namespace

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
-  :main ^:skip-aot clojure-noob.core
+  :main ^:skip-aot clojetris.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
              :dev {:dependencies [[midje "1.5.1"]]}}
