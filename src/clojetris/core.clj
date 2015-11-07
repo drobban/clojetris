9b4f2a47501f9f0c2482e63767931863071aba17
David Robertsson <David.robertsson@gmail.com>
2015-11-07 11:46:54 +0100

    gameboard state
    Added functions to save a brick into gameboard state.
    Next todo is to add a function that checks the sum of brick and state
    before and after an update. If sum isnt equal, an hit event have occured.

--- src/clojetris/core.clj
+++ src/clojetris/core.clj
@@ -8,7 +8,7 @@
 
 
 ;; Game brick bitmaps
-;; The bricks L, S is declared bellow
+;; The bricks L, S is declared below
 (def l_brick [[0 0 0 0]
               [0 1 0 0]
               [0 1 0 0]
@@ -19,9 +19,80 @@
               [1 1 0 0]
               [0 0 0 0]])
 
+;; Gameboard bitmap
+;; Should add a padding of 1 extra
+(def gameboard [[0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+                [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+                [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+                [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+                [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+                [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+                [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+                [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+                [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+                [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+                [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+                [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+                [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+                [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+                [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+                [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+                [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+                [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
+                [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]])
+
+;; Kom ihåg hur vi kan applicera händelser från en lista
+;; (reduce #(%2 %1) s_brick [rotate rotate rotate rotate])
+
+;; (def rotate (partial brick_rotation 1))
+;; (-> s_brick rotate rotate rotate)
 ;; Rotates the game bricks.
 (defn brick_rotation [n_times, colls]
   "takes 2 arguments, number of rotations and a symetric 2d list"
   (if (= n_times 0)
     colls
     (recur (dec n_times) (reverse (partition-all 4 (apply interleave colls))))))
+
+(defn trim_into [board_selection brick_selection pos]
+  "Trim into returns a new brick data that will fit into board at pos"
+  (if (< (count board_selection) (+ pos (count brick_selection)))
+    (subvec brick_selection
+            0
+            (max 0 (- (count brick_selection)
+                      (- (+ pos (count brick_selection))
+                         (count board_selection)))))
+    (subvec brick_selection
+            (max 0 (min (count brick_selection)
+                        (- (count brick_selection)
+                           (+ (count brick_selection) pos))))
+            (count brick_selection))))
+
+(defn mask_in [board_row brick_row pos_col]
+  "Merges a brick row into board row at a given column"
+  (let [beginning (subvec board_row
+                          0
+                          (max 0 pos_col))
+        selected (subvec board_row
+                         (max 0 pos_col)
+                         (+ (count (trim_into board_row brick_row pos_col)) (max 0 pos_col)))
+        end (subvec board_row
+                    (+ (max 0 pos_col) (count (trim_into board_row brick_row pos_col)))
+                    (count board_row))]
+    (vec (concat beginning (map bit-or selected (trim_into board_row brick_row pos_col)) end))))
+
+(defn brick_position [gameboard brick pos_row pos_col]
+  "Merges brick into gameboard at position row & col"
+  (let [game_row (min (count gameboard) (max 0 pos_row))
+        trimmed_brick (trim_into gameboard brick (min (count gameboard) pos_row))]
+    (loop [processed_rows (subvec gameboard 0 game_row)
+           selected_row (nth gameboard game_row)
+           rest_rows (subvec gameboard (+ game_row 1) (count gameboard))
+           row_idx 0]
+      (if (= row_idx (count trimmed_brick))
+        (if (nil? selected_row)
+          (into processed_rows rest_rows)
+          (into (conj processed_rows selected_row) rest_rows))
+        (recur (conj processed_rows (mask_in selected_row (nth trimmed_brick row_idx) pos_col))
+               (first rest_rows)
+               (rest rest_rows)
+               (inc row_idx))))))
--- test/clojetris/core_test.clj
+++ test/clojetris/core_test.clj
@@ -1,15 +1,57 @@
 (ns clojetris.core-test
   (:require [clojure.test :refer :all]
-            [clojetris.core :refer :all]))
+            [clojetris.core :refer :all]
+            [midje.sweet :refer [fact]]))
 
 (def rotated_l [[0 0 0 0]
                 [0 0 0 1]
                 [0 1 1 1]
                 [0 0 0 0]])
+
 (def rotated_s [[0 1 0 0]
                 [0 1 0 0]
                 [0 0 1 0]
                 [0 0 1 0]])
+
+(def lbrick1_board [[0 0 0 0 0 0]
+                    [0 0 1 0 0 0]
+                    [0 0 1 0 0 0]
+                    [0 0 1 1 0 0]
+                    [0 0 0 0 0 0]])
+
+(def lbrick2_board [[0 0 0 0 0 0]
+                    [1 0 0 0 0 0]
+                    [1 0 0 0 0 0]
+                    [1 1 0 0 0 0]
+                    [0 0 0 0 0 0]])
+
+(def lbrick3_board [[0 0 0 0 0 0]
+                    [0 0 0 0 0 0]
+                    [0 0 0 0 0 0]
+                    [1 0 0 0 0 0]
+                    [0 0 0 0 0 0]])
+
+(def lbrick4_board [[0 1 0 0 0 0]
+                    [0 1 0 0 0 0]
+                    [0 1 1 0 0 0]
+                    [0 0 0 0 0 0]
+                    [0 0 0 0 0 0]])
+
+(def lbrick5_board [[0 0 0 0 0 0]
+                    [0 0 0 0 0 0]
+                    [0 0 0 0 0 0]
+                    [0 1 0 0 0 0]
+                    [0 1 0 0 0 0]])
+
+(def test_board [[0 0 0 0 0 0]
+                 [0 0 0 0 0 0]
+                 [0 0 0 0 0 0]
+                 [0 0 0 0 0 0]
+                 [0 0 0 0 0 0]])
+
+(def test_row [0 0 0 0 0 0])
+(def test_b_row [0 1 0 0])
+
 
 (deftest bricks
   (testing "Rotation"
@@ -20,4 +62,19 @@
     (testing "S-Brick"
       (is (= (brick_rotation 1 s_brick) rotated_s))
       (is (= (brick_rotation 3 rotated_s) s_brick))
-      (is (= (brick_rotation 4 s_brick) s_brick)))))
+      (fact s_brick => (brick_rotation 4 s_brick))))
+  (testing "Brick movement"
+    (testing "Position brick on gameboard"
+      (fact (brick_position test_board l_brick 0 1) => lbrick1_board)
+      (fact (brick_position test_board l_brick 0 -1) => lbrick2_board)
+      (fact (brick_position test_board l_brick 0 -2) => lbrick3_board)
+      (fact (brick_position test_board l_brick 0 -3) => test_board)
+      (fact (brick_position test_board l_brick -3 -3) => test_board)
+      (fact (brick_position test_board l_brick -1 0) => lbrick4_board)
+      (fact (brick_position test_board l_brick 2 0) => lbrick5_board)
+      ))
+  (testing "Row trimming"
+    (fact (trim_into test_row test_b_row -1) => [1 0 0])
+    (fact (trim_into test_row test_b_row 4) => [0 1])
+    (fact (trim_into test_row test_b_row 5) => [0])
+    (fact (trim_into test_row test_b_row 6) => [])))
